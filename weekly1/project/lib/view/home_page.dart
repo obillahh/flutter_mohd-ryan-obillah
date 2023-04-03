539122292f2519c728c1cd1cba6bf83d9ab5bb7f
Mohd Ryan Obillah <ryanobillah@gmail.com>
2023-04-03 10:22:54 +0700

    menambah screenshot Code Compoetence 1

--- weekly1/project/lib/view/home_page.dart
+++ weekly1/project/lib/view/home_page.dart
@@ -20,31 +20,33 @@ class HomePage extends StatelessWidget {
       ),
       body: Container(
         margin: const EdgeInsets.symmetric(horizontal: 30),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              'Manage your freelance business with us!',
-              style: AppFont.primaryFont.headlineMedium,
-            ),
-            const SizedBox(height: 15),
-            Text(
-              'Takes less than 10 minutes to fill out all the information needed to register your business',
-              style: AppFont.primaryFont.bodyMedium,
-            ),
-            const SizedBox(height: 40),
-            const FormCustom(),
-            const SizedBox(height: 40),
-            Text(
-              'By signing up you are agreeing to our',
-              style: AppFont.primaryFont.bodyMedium,
-            ),
-            const SizedBox(height: 10),
-            Text(
-              'Terms and Conditions',
-              style: AppFont.primaryFont.labelMedium,
-            ),
-          ],
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'Manage your freelance business with us!',
+                style: AppFont.primaryFont.headlineMedium,
+              ),
+              const SizedBox(height: 15),
+              Text(
+                'Takes less than 10 minutes to fill out all the information needed to register your business',
+                style: AppFont.primaryFont.bodyMedium,
+              ),
+              const SizedBox(height: 40),
+              const FormCustom(),
+              const SizedBox(height: 40),
+              Text(
+                'By signing up you are agreeing to our',
+                style: AppFont.primaryFont.bodyMedium,
+              ),
+              const SizedBox(height: 10),
+              Text(
+                'Terms and Conditions',
+                style: AppFont.primaryFont.labelMedium,
+              ),
+            ],
+          ),
         ),
       ),
     );
